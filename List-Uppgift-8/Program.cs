bf7977c1b0d39b4a8224b2d4831f756ec87d10a9
Admin <Admin@TEB123018>
2024-02-07 11:04:04 +0100

    Add project files.

---
+++ List-Uppgift-8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Uppgift_8
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hur många koder vill du skriva in?");
+
+            int num = int.Parse(Console.ReadLine());
+
+            Dictionary<char, char> map = new();
+
+            Console.WriteLine("Skriv in koderna med mellanslag och tomma rader.");
+
+            for (int i = 0; i < num; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                map.Add(char.Parse(input[0]), char.Parse(input[1]));
+            }
+
+            Console.WriteLine("Vad vill du koda av?");
+            Console.WriteLine(Koda(map, Console.ReadLine()));
+
+        }
+
+        static string Koda(Dictionary<char, char> map, string msg)
+        {
+            string[] message = msg.Split("");
+            for (int i = 0; i < map.Count; i++)
+            {
+                
+            }
+        }
+    }
+}
